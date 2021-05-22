9a5f2ed11ebc7d4924b4e3c52dd4bf9ff0ac3bf1
Micahel Essiet <emsaa2002@gmail.com>
2021-05-22 22:04:44 +0100

    initial commit

--- src/script.js
+++ src/script.js
@@ -1,105 +1,129 @@
-import './style.css'
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import * as dat from 'dat.gui'
+const webpack = require('webpack');
 
-// Debug
-const gui = new dat.GUI()
+const fullname = document.getElementById("name");
+const form = document.getElementById("form");
+const error = document.getElementById("error");
+const profileimage = document.getElementById("profileimage");
+const githubname = document.getElementById("githubname");
+const username = document.getElementById("username");
+const profiledescription = document.getElementById("profiledescription");
+const followers = document.getElementById("followers");
+const following = document.getElementById("following");
+const stars = document.getElementById("stars");
+const repocol = document.getElementById('repocol');
+const reponumber = document.getElementById('reponumber')
+const repoitem = document.getElementById('repoitem')
+const repoitemtitle = document.getElementById('repoitemtitle')
+const repoitemtitlelink = document.getElementById('repoitemtitlelink')
+const repodesc = document.getElementById('repodesc')
+const repolang = document.getElementById('repolang')
+const repoupdatetime = document.getElementById('repoupdatetime')
+let repolist= []
 
-// Canvas
-const canvas = document.querySelector('canvas.webgl')
+let jsonresponse;
 
-// Scene
-const scene = new THREE.Scene()
-
-// Objects
-const geometry = new THREE.TorusGeometry( .7, .2, 16, 100 );
-
-// Materials
-
-const material = new THREE.MeshBasicMaterial()
-material.color = new THREE.Color(0xff0000)
-
-// Mesh
-const sphere = new THREE.Mesh(geometry,material)
-scene.add(sphere)
-
-// Lights
-
-const pointLight = new THREE.PointLight(0xffffff, 0.1)
-pointLight.position.x = 2
-pointLight.position.y = 3
-pointLight.position.z = 4
-scene.add(pointLight)
-
-/**
- * Sizes
- */
-const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
+class GetResponses {
+  static async fetchUser(username) {
+    return fetch(`https://api.github.com/users/${username}`)
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }
 }
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+async function requestUser(user) {
+  const usereg = "michaelessiet";
+  fetch("https://api.github.com/graphql", {
+    method: "POST",
+    headers: {
+      'Authorization':
+        "Bearer" + " " + "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",
+      "Content-Type": "application/graphql",
+    },
+    body: JSON.stringify({
+      query: `
+        query($loginname: String!){
+          user(login: $loginname){
+            login
+            name
+            avatarUrl
+            bio
+            followers(first: 10) {
+              totalCount
+            }
+            following(last:10){
+              totalCount
+            }
+            starredRepositories{
+              totalCount
+            }
+            repositories(last: 20) {
+              edges {
+                node {
+                  name
+                  url
+                  stargazerCount
+                  forkCount
+                  description
+                  updatedAt
+                  primaryLanguage{
+                    name
+                  }
+                  description
+                }
+              }
+            }
+          }
+        }`,
+      variables: {
+        "loginname": user
+      },
+    }),
+  })
+    .then((res) => {
+      repocol.innerHTML=''
+      return res.json()
+    })
+    .then((data) =>  {
+      githubname.innerText = data["data"]["user"]["name"]
+      username.innerText = data["data"]["user"]["login"]
+      profiledescription.innerText = data["data"]["user"]["bio"]
+      followers.innerText = data["data"]["user"]["followers"]['totalCount']
+      following.innerText = data["data"]["user"]["following"]['totalCount']
+      stars.innerText = data["data"]['user']['starredRepositories']['totalCount']
+      profileimage.setAttribute('src', data['data']['user']['avatarUrl'])
+      repolist = data["data"]['user']['repositories']['edges']
+      reponumber.innerText = repolist.length
+      console.log(repolist)
+      for(let i= 0; i<repolist.length; i++){
+        let listitem = document.createElement('div')
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+        repoitemtitlelink.innerText = repolist[i]['node']['name']
+        repoitemtitlelink.setAttribute('href', repolist[i]['node']['url'])
+        repodesc.innerText = repolist[i]['node']['description']
+        repoupdatetime.innerText = `Updated on ${new Date(repolist[i]['node']['updatedAt']).toDateString()}`
+        console.log(repolist[i]['node']['updatedAt'])
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-})
+        if( repolist[i]['node']['primaryLanguage'] !== null){
+          repolang.innerText = repolist[i]['node']['primaryLanguage']['name']
+        }
 
-/**
- * Camera
- */
-// Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 0
-camera.position.y = 0
-camera.position.z = 2
-scene.add(camera)
+        if( repolist[i]['node']['primaryLanguage'] == null){
+          repolang.innerText = ''
+        }
 
-// Controls
-// const controls = new OrbitControls(camera, canvas)
-// controls.enableDamping = true
-
-/**
- * Renderer
- */
-const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-
-/**
- * Animate
- */
-
-const clock = new THREE.Clock()
-
-const tick = () =>
-{
-
-    const elapsedTime = clock.getElapsedTime()
-
-    // Update objects
-    sphere.rotation.y = .5 * elapsedTime
-
-    // Update Orbital Controls
-    // controls.update()
-
-    // Render
-    renderer.render(scene, camera)
-
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
+        listitem.innerHTML = repoitem.outerHTML
+        repocol.appendChild(listitem) 
+      }
+    })
 }
 
-tick()
+form.addEventListener("submit", (e) => {
+  let messages = [];
+  if (name.value === "" || name.value == null || name.value === " ") {
+    messages.push("A username is required");
+  }
+
+  // GetResponses.fetchUser(document.querySelector("#name").value);
+  requestUser(document.querySelector('#name').value)
+  e.preventDefault();
+});
